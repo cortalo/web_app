c3c08200e0d7dbcb1e6929dce8189e4097fb363f
Cortalo <Cortalo@outlook.com>
2026-03-08 14:37:35 +0100

    init gin project

---
+++ settings/settings.go
@@ -0,0 +1,23 @@
+package settings
+
+import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+func Init() (err error) {
+	viper.SetConfigFile("config.yaml")
+	viper.AddConfigPath(".")
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("viper.ReadInConfig() failed with %s\n", err)
+		return
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		fmt.Printf("Config file changed: %s\n", in.Name)
+	})
+	return
+}
